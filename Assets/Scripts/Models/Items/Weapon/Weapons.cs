7d3306329e1c188bd7dab0a0ab75b80eec109a50
Gugu5021 <nico.piveteau@outlook.fr>
2024-02-19 16:55:18 +0100

    modified Weapon

--- Assets/Scripts/Models/Items/Weapon/Weapons.cs
+++ Assets/Scripts/Models/Items/Weapon/Weapons.cs
@@ -7,19 +7,6 @@ public class Weapons{
     private int damage;
     private int mp;
     private List<WeaponBuff> buffs = new List<WeaponBuff>();
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public int Damage{
         get => damage;
     }
